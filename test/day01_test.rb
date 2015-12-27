d1f33339fd4d35fe6708d746872779285a891e29
Eric Johnson <ejohnson.pub@gmail.com>
2015-12-26 22:53:14 -0800

    emj - prep for day02

--- test/day01_test.rb
+++ test/day01_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class Aoc2015Test < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Aoc2015::VERSION
-  end
-
   def day1
     test1 = '((foo))bar)(((('
     assert Aoc2015.day1 test1 == 3
---
+++ test/day02_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Aoc2015Test < Minitest::Test
+  def day02
+    assert true
+  end
+end
